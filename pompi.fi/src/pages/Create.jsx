4ddb58b3ae97f8185093e3c87be5d57e38854407
İzzet Emre Demir <izzetemredemir@gmail.com>
2024-06-01 23:39:25 +0200

    backend fix

--- pompi.fi/src/pages/Create.jsx
+++ pompi.fi/src/pages/Create.jsx
@@ -1,174 +1,236 @@
-import {useState} from 'react'
-import TokenLaunchpadAbi from "../abis/TokenLaunchpad.json"
-// const ethers = require("ethers")
-import { useWeb3ModalProvider, useWeb3ModalAccount } from '@web3modal/ethers/react'
-import { BrowserProvider, Contract, Interface } from 'ethers'
+import { useState } from "react";
+import TokenLaunchpadAbi from "../abis/TokenLaunchpad.json";
+import {
+  useWeb3ModalProvider,
+  useWeb3ModalAccount,
+} from "@web3modal/ethers/react";
+import { BrowserProvider, Contract, Interface } from "ethers";
 
 const Create = () => {
-    const [formData, setFormData] = useState({
-            name: '',
-            ticker: '',
-            description: '',
-            image: '',
-            telegramLink: '',
-        });
+  const [formData, setFormData] = useState({
+    name: "",
+    ticker: "",
+    description: "",
+    image: null,
+    telegramLink: "",
+  });
 
-    const { address, chainId, isConnected } = useWeb3ModalAccount()
-    const { walletProvider } = useWeb3ModalProvider()
+  const { address, chainId, isConnected } = useWeb3ModalAccount();
+  const { walletProvider } = useWeb3ModalProvider();
 
-    const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-          ...formData,
-          [name]: value,
-        });
-    };
-    
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "image") {
+      setFormData({
+        ...formData,
+        image: e.target.files[0],
+      });
+    } else {
+      setFormData({
+        ...formData,
+        [name]: value,
+      });
+    }
+  };
 
-    async function createToken(name, symbol) {
+  const uploadImage = async (image) => {
+    const imageData = new FormData();
+    imageData.append("files", image);
+
+    const response = await fetch(
+      "https://steadfast-prosperity-e813c26e87.strapiapp.com/api/upload",
+      {
+        method: "POST",
+        body: imageData,
+      }
+    );
+
+    const result = await response.json();
+    return result[0].id;
+  };
+
+  const createToken = async (name, symbol) => {
+    try {
+      const provider = new BrowserProvider(walletProvider);
+      await provider.send("eth_requestAccounts", []);
+      const signer = await provider.getSigner();
+      const tokenLaunchpadAddress =
+        "0x9c670237cfdE371eb6b2C250637d0a13A8b7a281";
+      const TokenLaunchpad = new Contract(
+        tokenLaunchpadAddress,
+        TokenLaunchpadAbi.abi,
+        signer
+      );
+
+      const tx = await TokenLaunchpad.createToken(name, symbol);
+      const receipt = await tx.wait();
+
+      const eventAbi = [
+        "event TokenCreated(address indexed tokenAddress, string name, string symbol)",
+      ];
+      const iface = new Interface(eventAbi);
+
+      let newTokenAddress;
+      for (let log of receipt.logs) {
         try {
-            // Connect to provider
-            const provider = new BrowserProvider(walletProvider);
-            await provider.send("eth_requestAccounts", []);
-        
-            // Get signer
-            const signer = await provider.getSigner();
-            console.log("Using account:", address);
-        
-            // Contract instance
-            const tokenLaunchpadAddress = "0x9c670237cfdE371eb6b2C250637d0a13A8b7a281";
-            const TokenLaunchpad = new Contract(
-                tokenLaunchpadAddress,
-                TokenLaunchpadAbi.abi,
-                signer
-            );
-            // Create token
-            const tx = await TokenLaunchpad.createToken(name, symbol);
-            const receipt = await tx.wait();
-        
-            console.log("Transaction receipt:", receipt);
-        
-            // Parse event
-            const eventAbi = [
-                "event TokenCreated(address indexed tokenAddress, string name, string symbol)",
-            ];
-            const iface = new Interface(eventAbi);
-        
-            let newTokenAddress;
-            for (let log of receipt.logs) {
-                try {
-                const parsedLog = iface.parseLog(log);
-                if (parsedLog.name === "TokenCreated") {
-                    newTokenAddress = parsedLog.args.tokenAddress;
-                    console.log("Token Address:", parsedLog.args.tokenAddress);
-                    console.log("Name:", parsedLog.args.name);
-                    console.log("Symbol:", parsedLog.args.symbol);
-                    break;
-                }
-                } catch (e) {
-                // log not parsed
-                }
-            }
-        } catch (error) {
-            console.log(`Error: ${error.message}`);
+          const parsedLog = iface.parseLog(log);
+          if (parsedLog.name === "TokenCreated") {
+            newTokenAddress = parsedLog.args.tokenAddress;
+            console.log("Token Address:", parsedLog.args.tokenAddress);
+            console.log("Name:", parsedLog.args.name);
+            console.log("Symbol:", parsedLog.args.symbol);
+            break;
+          }
+        } catch (e) {
+          // log not parsed
         }
       }
-      
+      return newTokenAddress;
+    } catch (error) {
+      console.log(`Error: ${error.message}`);
+    }
+  };
 
-      const handleSubmit = async (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const newTokenAddress = await createToken(formData.name, formData.ticker);
+      if (newTokenAddress) {
+        const imageId = await uploadImage(formData.image);
+        const response = await fetch(
+          "https://steadfast-prosperity-e813c26e87.strapiapp.com/api/memes",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              data: {
+                name: formData.name,
+                ticker: formData.ticker,
+                desc: formData.description,
+                address: newTokenAddress,
+                telegram: formData.telegramLink,
+                image: imageId,
+              },
+            }),
+          }
+        );
+        const result = await response.json();
+        console.log("Content created:", result);
+      } else {
+        console.error("Token creation failed, no address returned.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
-        await createToken(formData.name, formData.ticker)
-        // Handle form submission here
-        console.log(formData);
-      };
   return (
     <div className="w-full max-w-md mx-auto mt-5">
-    <form onSubmit={handleSubmit} className="bg-background px-8 pt-6 pb-8 mb-4">
-    <div className="mb-4">
-        <label className="block text-foreground text-sm font-bold mb-2" htmlFor="name">
-        name
-        </label>
-        <input
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-foreground leading-tight focus:outline-none focus:shadow-outline"
-        style={{backgroundColor: "#282A36"}}
-        id="name"
-        type="text"
-        name="name"
-        placeholder="Name"
-        value={formData.name}
-        onChange={handleChange}
-        />
+      <form
+        onSubmit={handleSubmit}
+        className="bg-background px-8 pt-6 pb-8 mb-4"
+      >
+        <div className="mb-4">
+          <label
+            className="block text-foreground text-sm font-bold mb-2"
+            htmlFor="name"
+          >
+            Name
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-foreground leading-tight focus:outline-none focus:shadow-outline"
+            style={{ backgroundColor: "#282A36" }}
+            id="name"
+            type="text"
+            name="name"
+            placeholder="Name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-foreground text-sm font-bold mb-2"
+            htmlFor="ticker"
+          >
+            Ticker
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-foreground leading-tight focus:outline-none focus:shadow-outline"
+            style={{ backgroundColor: "#282A36" }}
+            id="ticker"
+            type="text"
+            name="ticker"
+            placeholder="Ticker"
+            value={formData.ticker}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-foreground text-sm font-bold mb-2"
+            htmlFor="description"
+          >
+            Description
+          </label>
+          <textarea
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-foreground leading-tight focus:outline-none focus:shadow-outline"
+            style={{ backgroundColor: "#282A36" }}
+            id="description"
+            name="description"
+            placeholder="Description"
+            value={formData.description}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-foreground text-sm font-bold mb-2"
+            htmlFor="image"
+          >
+            Image
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-foreground leading-tight focus:outline-none focus:shadow-outline"
+            style={{ backgroundColor: "#282A36" }}
+            id="image"
+            type="file"
+            name="image"
+            accept="image/*"
+            onChange={handleChange}
+          />
+        </div>
+        <div className="mb-4">
+          <label
+            className="block text-foreground text-sm font-bold mb-2"
+            htmlFor="telegramLink"
+          >
+            Telegram Link
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-foreground leading-tight focus:outline-none focus:shadow-outline"
+            style={{ backgroundColor: "#282A36" }}
+            id="telegramLink"
+            type="text"
+            name="telegramLink"
+            placeholder="Telegram Link"
+            value={formData.telegramLink}
+            onChange={handleChange}
+          />
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-purple-900 hover:bg-pink mt-2 font-bold py-2 px-4 w-full"
+            type="submit"
+          >
+            Submit
+          </button>
+        </div>
+      </form>
     </div>
-    <div className="mb-4">
-        <label className="block text-foreground text-sm font-bold mb-2" htmlFor="ticker">
-        ticker
-        </label>
-        <input
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-foreground leading-tight focus:outline-none focus:shadow-outline"
-        style={{backgroundColor: "#282A36"}}
-        id="ticker"
-        type="text"
-        name="ticker"
-        placeholder="Ticker"
-        value={formData.ticker}
-        onChange={handleChange}
-        />
-    </div>
-    <div className="mb-4">
-        <label className="block text-foreground text-sm font-bold mb-2" htmlFor="description">
-        description
-        </label>
-        <textarea
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-foreground leading-tight focus:outline-none focus:shadow-outline"
-        style={{backgroundColor: "#282A36"}}
-        id="description"
-        name="description"
-        placeholder="Description"
-        value={formData.description}
-        onChange={handleChange}
-        />
-    </div>
-    <div className="mb-4">
-        <label className="block text-foreground text-sm font-bold mb-2" htmlFor="image">
-        image
-        </label>
-        <input
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-foreground leading-tight focus:outline-none focus:shadow-outline"
-        style={{backgroundColor: "#282A36"}}
-        id="image"
-        type="file"
-        name="image"
-        accept="image/*"
-        onChange={handleChange}
-        />
-    </div>
-    <div className="mb-4">
-        <label className="block text-foreground text-sm font-bold mb-2" htmlFor="telegramLink">
-        telegram link
-        </label>
-        <input
-        className="shadow appearance-none border rounded w-full py-2 px-3 text-foreground leading-tight focus:outline-none focus:shadow-outline"
-        style={{backgroundColor: "#282A36"}}
-        id="telegramLink"
-        type="text"
-        name="telegramLink"
-        placeholder="Telegram Link"
-        value={formData.telegramLink}
-        onChange={handleChange}
-        />
-    </div>
-    <div className="flex items-center justify-between">
-        <button
-        className="bg-purple-900 hover:bg-pink mt-2 font-bold py-2 px-4 w-full"
-        type="submit"
-        >
-        Submit
-        </button>
-    </div>
-    </form>
-</div>
-    )
-}
+  );
+};
 
-export default Create
+export default Create;
--- pompi.fi/src/pages/Home.jsx
+++ pompi.fi/src/pages/Home.jsx
@@ -1,75 +1,105 @@
 import Token from "../components/Token";
 import { useEffect, useState } from "react";
 
-const data = [
-    {
-        address: "0xCD9bf60062093860f3698e9214D4D71Ba68258F3",
-        logoUrl:"https://assets.coingecko.com/coins/images/29850/large/pepe-token.jpeg?1696528776",
-        name:"pepe",
-        ticker:"$PP",
-        description:"a fun fren wit no utility",
-    },
-    {
-        address: "0x1",
-        logoUrl:"https://assets.coingecko.com/coins/images/29850/large/pepe-token.jpeg?1696528776",
-        name:"pepe2",
-        ticker:"$PP",
-        description:"a fun fren wit no utility",
-    },
-    {
-        address: "0x2",
-        logoUrl:"https://assets.coingecko.com/coins/images/29850/large/pepe-token.jpeg?1696528776",
-        name:"pepe3",
-        ticker:"$PP",
-        description:"a fun fren wit no utility",
-    },
-    {
-        address: "0x3",
-        logoUrl:"https://assets.coingecko.com/coins/images/29850/large/pepe-token.jpeg?1696528776",
-        name:"pepe",
-        ticker:"$PP",
-        description:"a fun fren wit no utility",
-    },
-]
+// const data = [
+//     {
+//         address: "0xCD9bf60062093860f3698e9214D4D71Ba68258F3",
+//         logoUrl:"https://assets.coingecko.com/coins/images/29850/large/pepe-token.jpeg?1696528776",
+//         name:"pepe",
+//         ticker:"$PP",
+//         description:"a fun fren wit no utility",
+//     },
+//     {
+//         address: "0x1",
+//         logoUrl:"https://assets.coingecko.com/coins/images/29850/large/pepe-token.jpeg?1696528776",
+//         name:"pepe2",
+//         ticker:"$PP",
+//         description:"a fun fren wit no utility",
+//     },
+//     {
+//         address: "0x2",
+//         logoUrl:"https://assets.coingecko.com/coins/images/29850/large/pepe-token.jpeg?1696528776",
+//         name:"pepe3",
+//         ticker:"$PP",
+//         description:"a fun fren wit no utility",
+//     },
+//     {
+//         address: "0x3",
+//         logoUrl:"https://assets.coingecko.com/coins/images/29850/large/pepe-token.jpeg?1696528776",
+//         name:"pepe",
+//         ticker:"$PP",
+//         description:"a fun fren wit no utility",
+//     },
+// ]
 
 const Home = () => {
-    const [search, setSearch] = useState('');
-    const [tokens, setTokens] = useState(data);
-    const handleSearch = () => {
-        setTokens(data.filter((token) => token.address.includes(search) || token.name.includes(search)))
+  const [search, setSearch] = useState("");
+  const [tokens, setTokens] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://steadfast-prosperity-e813c26e87.strapiapp.com/api/memes"
+      );
+      const result = await response.json();
+      const data = result.data.map((item) => ({
+        address: item.attributes.address,
+        logoUrl:
+          "https://assets.coingecko.com/coins/images/29850/large/pepe-token.jpeg?1696528776",
+        name: item.attributes.name,
+        ticker: item.attributes.ticker,
+        description: item.attributes.desc || "No description available",
+      }));
+      setTokens(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
     }
-    useEffect(() =>{
-        handleSearch()
-    }, [search])
-    return (
+  };
+
+  const handleSearch = () => {
+    setTokens((prevTokens) =>
+      prevTokens.filter(
+        (token) => token.address.includes(search) || token.name.includes(search)
+      )
+    );
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  useEffect(() => {
+    handleSearch();
+  }, [search]);
+
+  return (
     <div className="page-layout">
-        <input
+      <input
         className="shadow appearance-none border rounded w-full py-2 px-3 text-foreground leading-tight focus:outline-none focus:shadow-outline mt-5 mb-1"
-        style={{backgroundColor: "#282A36"}}
+        style={{ backgroundColor: "#282A36" }}
         id="name"
         type="text"
         name="name"
         placeholder="search by token name, or token address"
         value={search}
-        onChange={e => setSearch(e.target.value)}
-        />
-        <div className="grid grid-cols-3">
-            {tokens.map((token) => {
-                return (
-                    <Token 
-                        logoUrl={token.logoUrl}
-                        name={token.name}
-                        ticker={token.ticker}
-                        description={token.description}
-                        address={token.address}
-                        key={token.address}
-                    />
-                )
-            })}
-        </div>
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <div className="grid grid-cols-3">
+        {tokens.map((token) => {
+          return (
+            <Token
+              logoUrl={token.logoUrl}
+              name={token.name}
+              ticker={token.ticker}
+              description={token.description}
+              address={token.address}
+              key={token.address}
+            />
+          );
+        })}
+      </div>
     </div>
-    );
-  };
-  
-  export default Home;
-  
+  );
+};
+
+export default Home;
